4d1ea0ea5fa6caa4d3ca90e954754dce41ffd4f0
Mirko <mirkocostantini@gmail.com>
2022-06-18 16:22:20 +0200

    Creazione e sviluppo della sezione di registrazione vaccinazione, in attesa di classe di supporto per database

--- src/main/java/app/CentriVaccinali/RegCittVaccinatoController.java
+++ src/main/java/app/CentriVaccinali/RegCittVaccinatoController.java
@@ -1,5 +1,44 @@
 package app.CentriVaccinali;
 
+import app.TipoCentroVaccinale;
+import app.TipoVaccino;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+
 public class RegCittVaccinatoController {
-    
+
+    @FXML
+    private ChoiceBox<TipoVaccino> chb_vaccino;
+    @FXML
+    private TextField tf_centroV;
+    @FXML
+    private TextField tf_nome;
+    @FXML
+    private TextField tf_cognome;
+    @FXML
+    private TextField tf_codiceFiscale;
+    @FXML
+    private DatePicker dp_data;
+
+    @FXML
+    private void onEnterPressed() throws IOException {
+
+        System.out.println("Enter button pressed");
+        System.out.println(tf_cognome.getText()+" "+tf_nome.getText()+" vaccinato presso il centro "+tf_centroV.getText()+ " in data "+dp_data.getValue());
+        System.out.println("CF: "+tf_codiceFiscale.getText());
+        System.out.println("Vaccinato con una dose di "+chb_vaccino.getValue());
+    }
+
+
+    public void initialize() {
+        chb_vaccino.setItems(FXCollections.observableList(Arrays.asList(TipoVaccino.values())));
+        dp_data.setValue(LocalDate.now());
+    }
 }
