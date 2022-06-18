eaabab0f53bb291a200d312d4f53cad22498d458
Mirko <mirkocostantini@gmail.com>
2022-06-18 17:11:21 +0200

    Primo tentativo per la creazione del codice univoco da 16 bit per la vaccinazione

--- src/main/java/app/CentriVaccinali/RegCittVaccinatoController.java
+++ src/main/java/app/CentriVaccinali/RegCittVaccinatoController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 public class RegCittVaccinatoController {
@@ -34,8 +35,21 @@ public class RegCittVaccinatoController {
         System.out.println(tf_cognome.getText()+" "+tf_nome.getText()+" vaccinato presso il centro "+tf_centroV.getText()+ " in data "+dp_data.getValue());
         System.out.println("CF: "+tf_codiceFiscale.getText());
         System.out.println("Vaccinato con una dose di "+chb_vaccino.getValue());
+        System.out.println("16bit univoco: "+ stringKeyFormatter().hashCode());
+        
     }
 
+    private String stringKeyFormatter(){
+        String tmp ="";
+        //due del nome del centro, due del nome, due del cognome, datanascita del cf, data somministrazione ddMMaa, due del vaccino
+        tmp+=tf_centroV.getText().substring(0,2);
+        tmp+=tf_nome.getText().substring(0,2);
+        tmp+=tf_cognome.getText().substring(0,2);
+        tmp+=tf_codiceFiscale.getText().substring(6,8);
+        tmp+=dp_data.getValue().format(DateTimeFormatter.ofPattern("ddMMYY"));
+        tmp+=chb_vaccino.getValue().toString().substring(0,2);
+        return tmp;
+    }
 
     public void initialize() {
         chb_vaccino.setItems(FXCollections.observableList(Arrays.asList(TipoVaccino.values())));
