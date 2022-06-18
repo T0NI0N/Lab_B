f37a5e1bfef8613b961609507df7bc8f98d913bd
antonio <antoniogaudiano@outlook.com>
2022-06-18 14:30:34 +0200

    aggiunta gestione all'uscita dell'applicazione

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,18 @@
 package app;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * JavaFX App
@@ -14,12 +20,33 @@ import java.io.IOException;
 public class Main extends Application {
 
     private static Scene scene;
+    private static Stage primaryStage;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         scene = new Scene(loadFXML("Home"));
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+            @Override
+            public void handle(WindowEvent event) {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Uscire");
+                alert.setHeaderText("");
+                alert.setContentText("Sei sicuro di voler uscire dal programma?");
+                Optional<ButtonType> result = alert.showAndWait();
+                
+                if(result.get() == ButtonType.OK) {
+                    System.out.println("Closing...");
+                    System.exit(0);
+                }
+                else
+                    event.consume();
+            }
+            
+        });
     }
 
     public static void setRoot(String fxml) throws IOException {
