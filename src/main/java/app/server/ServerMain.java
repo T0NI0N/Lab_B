674922930d1d36ab877298d91c525f3fa7ea1c56
antonio <antoniogaudiano@outlook.com>
2022-06-22 20:39:35 +0200

    risolto errore che impediva il run del server

--- src/main/java/app/server/ServerMain.java
+++ src/main/java/app/server/ServerMain.java
@@ -5,16 +5,21 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class ServerMain {
+    private static Registry registry;
+
     public static void main(String[] args) {
         try {
-
+            System.out.println("Creating stub...");
             DatabaseHandler obj = new DatabaseHandler();
             ConnectionHandlerInterface stub = (ConnectionHandlerInterface) UnicastRemoteObject.exportObject(obj, 0);
-            Registry registry = LocateRegistry.createRegistry(1099);
-            registry.rebind("Balancer", stub);
-            System.out.println("Serverready");
+            System.out.println("Creating registry...");
+            registry = LocateRegistry.createRegistry(1099);
+            System.out.println("Binding service...");
+            registry.rebind("dbconnection", stub);
+            System.out.println("Server ready and running");
         } catch (Exception e) {
-            System.out.println("Server exception: " + e.toString());
+            System.out.print("Server error: ");
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module demo {
 
     requires javafx.fxml;
     requires javafx.controls;
+    requires java.rmi;
 
     opens app.cittadini to javafx.fxml;
     opens app.CentriVaccinali to javafx.fxml;
     opens app to javafx.graphics;
+
+    exports app.server;
 }
